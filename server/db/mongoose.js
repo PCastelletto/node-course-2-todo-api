08a05b43bbdf79cfd564e6c78b2b034b644a199b
Pablo Castelletto <pcastelletto7@gmail.com>
2017-12-26 19:53:39 -0300

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // configure to use promises
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+mongoose.connect('mongodb://Nyepz:test@ds131697.mlab.com:31697/udemy-node-course');
+// 'mongodb://localhost:27017/TodoApp'
 module.exports = {
   mongoose
 }
